c814c54671de71e6c6a75390195581311c8e26ff
Abena Adu <abeanadu@gmail.com>
2024-02-29 23:38:53 -0500

    Created lists subjects, grades, and gradebook

---
+++ gradebook.py
@@ -0,0 +1,8 @@
+#Name of subjects
+subjects = ["physics", "calculus", "poetry", "history"]
+
+#Grades
+grades = [98, 97, 85, 88]
+
+#Combination of subjects and grades 
+gradebook = [["physics", 98], ["calculus", 97], ["poetry", 85], ["history", 88]]
