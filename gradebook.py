3d1d0bf93733b4374ea6a4ce58e4b1e3d494d92e
Abena Adu <abeanadu@gmail.com>
2024-03-01 01:16:34 -0500

    Created one large gradebook

--- gradebook.py
+++ gradebook.py
@@ -10,13 +10,26 @@ gradebook = [["physics", 98],
              ["poetry", 85], 
              ["history", 88]]
 
+last_semester_gradebook = [["politics", 80], 
+                           ["latin", 96], 
+                           ["dance", 97], 
+                           ["architecture", 65]]
+
 #Adding new subject and grade
 gradebook.append(["computer science", 100])
+print(gradebook)
 #Adding new subject and grade
 gradebook.append(["visual arts", 93])
+print(gradebook)
 #Added 5 points to the visual arts grade
 gradebook[5][1] = 98
+print(gradebook)
 #Removed grade for poetry class
 gradebook[2].remove(85)
+print(gradebook)
 #Added pass value to poetry class
 gradebook[2].append("Pass")
+print(gradebook)
+#Combining last semester gradebook and gradebook
+full_gradebook = last_semester_gradebook + gradebook
+print(full_gradebook)
